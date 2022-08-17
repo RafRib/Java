512b22376ca6d1b8d99f193b2bf68f29cce876ca
Rafael Ribeiro <RafaelR@tjserrano>
2022-08-16 21:18:11 -0300

    Primeiro commit

---
+++ userDept/src/main/java/com/example/userDept/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.userDept.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.userDept.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+
+}
---
+++ userDept/src/test/java/com/example/userDept/UserDeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userDept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
